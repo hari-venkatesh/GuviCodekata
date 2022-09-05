a46aaaa171701e55ff63e9edc3a37143fb702446
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-09-05 23:45:39 +0530

    Code Snippets

---
+++ AbsoluteBeginner/Program19.py
@@ -0,0 +1,26 @@
+
+# Write a code to get the input and 
+# print it 5 times.
+
+# Input Description:
+# A single line contains an integer N.
+
+# Output Description:
+# Output contains 5 lines with each line 
+# having the value N.
+
+
+n=int(input())
+for i in range(5):
+    print(n)
+
+
+
+# Sample Input :
+# 4
+# Sample Output :
+# 4
+# 4
+# 4
+# 4
+# 4
