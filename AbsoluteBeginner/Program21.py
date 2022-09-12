65a95878ee90e2e3663cb12a347c448136e49b57
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-09-12 23:49:19 +0530

    Practice Codes

---
+++ AbsoluteBeginner/Program21.py
@@ -0,0 +1,27 @@
+
+# You are provided with two numbers. 
+# Find and print the smaller number.
+
+# Input Description:
+# You are provided with two numbers as input.
+
+# Output Description:
+# Print the small number out of the 
+# two numbers.
+
+
+
+a,b=map(int,input().split())
+
+if (a>b):
+    print(b)
+else:
+    print(a)
+
+
+
+
+# Sample Input :
+# 23 1
+# Sample Output :
+# 1
