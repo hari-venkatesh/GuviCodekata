32ebdf8c631068de8c5bb1801cbfb497c90e31d7
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-09-21 22:35:36 +0530

    Positive Integer

---
+++ AbsoluteBeginner/Program24.py
@@ -0,0 +1,25 @@
+
+# You are given with a number "N", 
+# find its cube.
+
+# Input Description:
+# A positive integer is provided.
+
+# Output Description:
+# Find the cube of the number.
+
+
+
+n = int(input())
+def Cube(n):
+    k=n*n*n
+    print(k)
+    
+Cube(n)
+
+
+
+# Sample Input :
+# 2
+# Sample Output :
+# 8
