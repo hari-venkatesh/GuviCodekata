9778e5af3d2b866ebe598b5732e9a9bfd6e31798
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-09-23 22:10:45 +0530

    Python Code Snippets

---
+++ AbsoluteBeginner/Program29.py
@@ -0,0 +1,24 @@
+
+# Write a program to get a string as 
+# input and reverse the string without 
+# using temporary variable.
+
+# Input Description:
+# A single line containing a string.
+
+# Output Description:
+# Print the reversed string.
+
+
+
+s = input()
+k = s[::-1]
+print(k)
+
+
+
+
+# Sample Input :
+# GUVI
+# Sample Output :
+# IVUG
